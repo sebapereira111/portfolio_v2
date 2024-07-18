609ea06431fc03780866952a3b05653f363450b4
Sebastian Pereira <157150645+sebapereira111@users.noreply.github.com>
2024-07-18 17:55:41 -0400

    Some html and css done

---
+++ resources/javascript/animation.js
@@ -0,0 +1,5 @@
+const header = document.getElementsByTagName("header");
+const nav = document.getElementsByTagName("nav");
+
+const headerStyles = window.getComputedStyle(header[0]);
+const navStyles = window.getComputedStyle(nav[0]);

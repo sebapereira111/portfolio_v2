1b194ed93c1ce74ea479956b9d30d37719c8e8df
Sebastian Pereira <157150645+sebapereira111@users.noreply.github.com>
2024-07-24 08:45:44 -0400

    Redoing animations

--- resources/javascript/animation.js
+++ resources/javascript/animation.js
@@ -1,20 +1,24 @@
+/*
+const contenido = document.getElementsByClassName('contenido');
+const navItem = document.getElementsByClassName("nav-item");
+
 const observer = new IntersectionObserver(
     (entries) => {
         for (const entry of entries) {
             if (entry.isIntersecting) {
                 entry.target.style.opacity = "1";
+                navItem[entry.target.index].style.backgroundColor = "var(--selected)";
             } else {
                 entry.target.style.opacity = "0";
+                navItem[entry.target.index].style.backgroundColor = "var(--background)";
             }
         }
     },
     { threshold: 0.6 }
 );
-  
-//const contenido = document.getElementById('acerca-de-mi');
-const contenido = document.getElementsByClassName('contenido');
 
 for (i = 0 ; i < contenido.length ; i++) {
     observer.observe(contenido[i]);
 }
 
+*/

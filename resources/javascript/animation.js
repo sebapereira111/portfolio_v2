4880fa368e5159f1d5b0fa1cce5e1b59b7c5e78b
Sebastian Pereira <157150645+sebapereira111@users.noreply.github.com>
2024-07-24 19:48:32 -0400

    Nav indicators animation

--- resources/javascript/animation.js
+++ resources/javascript/animation.js
@@ -1,14 +1,28 @@
 // Fade in animations using intersection observer
-const contenido = document.getElementsByClassName('contenido');
-const navItem = document.getElementsByClassName("nav-item");
+const contenido = document.getElementsByClassName('visible');
+const navItem = document.querySelectorAll(".nav-item");
 
 const observer = new IntersectionObserver(
     (entries) => {
         for (const entry of entries) {
             if (entry.isIntersecting) {
                 entry.target.style.opacity = "1";
+                for (let i = 0; i < navItem.length; i++) {
+                    if (("nav-" + entry.target.id) == navItem[i].querySelector("a").id) {
+                        navItem[i].querySelector("a").style.fontWeight = "bold";
+                    } else if (entry.target.id == "fotomatic") {
+                        navItem[1].querySelector("a").style.fontWeight = "bold";
+                    }
+                }
             } else {
                 entry.target.style.opacity = "0";
+                for (let i = 0; i < navItem.length; i++) {
+                    if (("nav-" + entry.target.id) == navItem[i].querySelector("a").id) {
+                        navItem[i].querySelector("a").style.fontWeight = "normal";
+                    } else if (entry.target.id == "fotomatic") {
+                        navItem[1].querySelector("a").style.fontWeight = "normal";
+                    }
+                }
             }
         }
     },
@@ -18,4 +32,6 @@ const observer = new IntersectionObserver(
 for (i = 0 ; i < contenido.length ; i++) {
     observer.observe(contenido[i]);
 }
+
+
 

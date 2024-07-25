3f4d3b130f3c36f618f6e34ba4e4370333566c26
Sebastian Pereira <157150645+sebapereira111@users.noreply.github.com>
2024-07-24 23:47:31 -0400

    Add Menu on mobile

--- resources/javascript/animation.js
+++ resources/javascript/animation.js
@@ -1,6 +1,7 @@
 // Fade in animations using intersection observer
-const contenido = document.getElementsByClassName('visible');
+const contenido = document.querySelectorAll('.visible');
 const navItem = document.querySelectorAll(".nav-item");
+
 
 const observer = new IntersectionObserver(
     (entries) => {
@@ -10,8 +11,10 @@ const observer = new IntersectionObserver(
                 for (let i = 0; i < navItem.length; i++) {
                     if (("nav-" + entry.target.id) == navItem[i].querySelector("a").id) {
                         navItem[i].querySelector("a").style.fontWeight = "bold";
+                        navItem[i].classList.toggle("mostrar-nav-item", true);
                     } else if (entry.target.id == "fotomatic") {
                         navItem[1].querySelector("a").style.fontWeight = "bold";
+                        navItem[1].classList.toggle("mostrar-nav-item", true);
                     }
                 }
             } else {
@@ -19,8 +22,10 @@ const observer = new IntersectionObserver(
                 for (let i = 0; i < navItem.length; i++) {
                     if (("nav-" + entry.target.id) == navItem[i].querySelector("a").id) {
                         navItem[i].querySelector("a").style.fontWeight = "normal";
+                        navItem[i].classList.toggle("mostrar-nav-item", false);
                     } else if (entry.target.id == "fotomatic") {
                         navItem[1].querySelector("a").style.fontWeight = "normal";
+                        navItem[1].classList.toggle("mostrar-nav-item", false);
                     }
                 }
             }
@@ -35,3 +40,25 @@ for (i = 0 ; i < contenido.length ; i++) {
 
 
 
+
+
+// menu on mobile
+const botonMenu = document.querySelector("#boton-menu");
+const menu = document.querySelector("#menu");
+const menuUl = menu.querySelector("ul");
+const navInicio = menu.querySelector(".nav-inicio");
+
+const openMenu = function() {
+    console.log("Se ha abierto el menu!!!");
+    menu.classList.toggle("menu", true);
+}
+
+const closeMenu = function() {
+    console.log("Se ha abierto el menu!!!");
+    menu.classList.toggle("menu", false);
+}
+
+botonMenu.addEventListener("click", openMenu);
+
+menuUl.addEventListener("click", closeMenu);
+navInicio.addEventListener("click", closeMenu);
